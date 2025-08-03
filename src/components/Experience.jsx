d1a175d2f45e72808c3aed6db5dbac002606dce7
HasnainAli313 <aliorakzai407@.com>
2025-08-03 20:42:39 +0500

    updated experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,5 @@
-import exp1 from "../assets/young.jpeg"
+import exp1 from "../assets/nuexus_logo.jpeg"
+import exp2 from "../assets/young.jpeg"
 function Experience() {
   return (
     <section className="p-5 bg-[#111827]">
@@ -7,20 +8,37 @@ function Experience() {
         <p className="text-normal m-5 text-lg md:text-xl   text-center">Here is a quick summary of my most recent experiences:</p>
         <div  data-aos="fade-up"
      data-aos-duration="1000">
+
+           {/* Experience card 1 */}  
             <div className='rounded-xl bg-[#1F2937] flex sm:flex-row gap-1 flex-col items-center p-5  '>
-                {/* Experience cards */}  
-            <img  className="pr-[80px]" src={exp1} alt="" />
+            <img  className="pr-[80px] w-[250px]" src={exp1} alt="" />
             <div className="md:w-[90%]  flex sm:flex-row gap-5 md:justify-around  flex-col ">
                 <div>
                 <h1 className="p-3  text-xl font-semibold">Frontend Web Developer</h1>
-                <p className="pl-3 w-[90%]">As a Frontend Web Developer, I have polished my HTML, CSS, and responsive design skills by completing the tasks.
+                <p className="pl-3 w-[90%]">As a Frontend Web Developer, I have polished my HTML, CSS, JavaScript , React and TailwindCSS responsive design skills by completing the projects.
                     
                       </p>
                 </div>
-            <p className=" w-[100%] pl-3 pt-3  ">Jan 2024-Feb 2024</p>
+            <p className=" w-[100%] pl-3 pt-3  ">Oct 2024 - Dec 2024</p>
             </div>
 
             </div>
+
+                {/* Experience card 2*/}  
+            <div className='rounded-xl bg-[#1F2937] flex mt-5  sm:flex-row gap-1 flex-col items-center p-5  '>
+            <img  className="pr-[80px]" src={exp2} alt="" />
+            <div className="md:w-[90%]  flex sm:flex-row gap-5 md:justify-around  flex-col ">
+                <div>
+                <h1 className="p-3  text-xl font-semibold">Frontend Web Developer</h1>
+                <p className="pl-3 w-[90%]">As a Frontend Web Developer, I have polished my HTML, CSS, and responsive design skills by completing the projects.
+                    
+                      </p>
+                </div>
+            <p className=" w-[100%] pl-3 pt-3  ">Jan 2024 - Feb 2024</p>
+            </div>
+
+            </div>
+
         </div>
     </section>
   )
